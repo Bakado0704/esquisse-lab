8a5edb6b6f6b81b37147e595d62f15c0b379b41e
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-03-18 00:30:07 +0900

    [feature]ページの初期設定

---
+++ src/app/_app.tsx
@@ -0,0 +1,10 @@
+import { HeaderLayout } from "@/components/layout/HeaderLayout";
+import type { AppProps } from "next/app";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <HeaderLayout>
+      <Component {...pageProps} />
+    </HeaderLayout>
+  );
+}
