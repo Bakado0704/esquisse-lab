5f78c46047b0610af24da2da09dbe4f3e503c4e6
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-03-21 00:21:44 +0900

    [fix]membersのボタンを追加

--- src/components/pages/common/NavHeader/NavHeader.tsx
+++ src/components/pages/common/NavHeader/NavHeader.tsx
@@ -3,11 +3,16 @@ import { FlexBox } from "@/components/common/FlexBox";
 import logo from "@/assets/logo/esquisse-lab.png";
 import Image from "next/image";
 
+import styles from "./NavHeader.module.scss";
+
 const NavHeader = () => {
   return (
     <FlexBox justifyContent="space-between" alignItems="center" height="6rem">
       <Image src={logo} alt="logo" height={40} />
-      <Button>ログイン</Button>
+      <FlexBox gap="2.4rem">
+        <button className={styles.memberButton}>Members</button>
+        <Button>ログイン</Button>
+      </FlexBox>
     </FlexBox>
   );
 };
