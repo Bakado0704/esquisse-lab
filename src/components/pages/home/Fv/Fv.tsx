5b682acc238373000c08e121ecfe0c5574ce49ae
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-03-21 00:21:04 +0900

    [fix]fvのコンポーネント化

---
+++ src/components/pages/home/Fv/Fv.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/common/Button";
+import { FlexBox } from "@/components/common/FlexBox";
+import Image from "next/image";
+import fvImg from "@/assets/fv/fv.png";
+import { Typography } from "@/components/common/Typography";
+
+import styles from "./fv.module.scss";
+import { Icon } from "@/components/common/Icon";
+
+const Fv = () => {
+  return (
+    <FlexBox className={styles.container}>
+      <FlexBox className={styles.imageContainer}>
+        <Image
+          src={fvImg}
+          alt="fv"
+          height={800}
+          style={{ objectFit: "cover" }}
+        />
+      </FlexBox>
+      <FlexBox flexDirection="column" justifyContent="center" gap="2.4rem">
+        <Typography fontSize="8rem" lineHeight="130%" gothic color="w1">
+          Hokudai
+          <br />
+          Esquisse
+          <br />
+          Forum
+        </Typography>
+        <Icon iconName="ArrowLarge" />
+        <Button theme="rectPink" size="large" className={styles.button}>
+          投稿する
+        </Button>
+      </FlexBox>
+    </FlexBox>
+  );
+};
+export default Fv;
---
+++ src/components/pages/home/Fv/index.ts
@@ -0,0 +1,1 @@
+export { default as Fv } from "./Fv";
