10d21312d10d4929083a3280058a22f686988005
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-03-23 01:31:54 +0900

    [fix]appからheaderLayoutを削除

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,11 +1,5 @@
-import { HeaderLayout } from "@/components/layout/HeaderLayout";
-
 import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <HeaderLayout>
-      <Component {...pageProps} />
-    </HeaderLayout>
-  );
+  return <Component {...pageProps} />;
 }
