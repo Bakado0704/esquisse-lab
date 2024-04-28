6b997e727602ea3a6a4dc06765ff32b249556400
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-04-28 23:21:42 +0900

    [fix]titleのscss変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>
+        <script> </script>
         <MemberProvider>
           <HeaderLayout>{children}</HeaderLayout>
         </MemberProvider>
