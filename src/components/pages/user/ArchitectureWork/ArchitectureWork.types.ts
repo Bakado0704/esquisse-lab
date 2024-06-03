d585ec033faf901bfd7b61fc26d3bc6e631eb6b8
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-06-04 00:56:01 +0900

    [fix]ArchitectureWorkコンポーネントを実装

---
+++ src/components/pages/user/ArchitectureWork/ArchitectureWork.types.ts
@@ -0,0 +1,5 @@
+import { Work } from "@/types/application/work.types";
+
+export type ArchitectureWorkProps = {
+  archiWork: Work[];
+};
---
+++ src/components/pages/user/ArchitectureWork/index.ts
@@ -0,0 +1,1 @@
+export { default as ArchitectureWork } from "./ArchitectureWork";
