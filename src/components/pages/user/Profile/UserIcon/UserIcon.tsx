4f78e1c6d9f7cd5191566f38558b46a0f1f0a74b
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-06-04 00:52:47 +0900

    [feature]profileコンポーネントを実装

---
+++ src/components/pages/user/Profile/UserIcon/UserIcon.tsx
@@ -0,0 +1,25 @@
+import { useRouter } from "next/navigation";
+
+import { FlexBox } from "@/components/common";
+import { MemberIconProps } from "@/components/pages/common/Members/MemberIcon/MemberIcon.types";
+
+import styles from "./UserIcon.module.scss";
+
+const MemberIcon = ({ user }: MemberIconProps) => {
+  const router = useRouter();
+  return (
+    <FlexBox
+      justifyContent="center"
+      className={styles.iconContainer}
+      onClick={() => router.push(`/member/${user.id}/edit`)}
+    >
+      <div className={styles.background} />
+      <div className={styles.icon}>
+        <span />
+        <span />
+      </div>
+    </FlexBox>
+  );
+};
+
+export default MemberIcon;
