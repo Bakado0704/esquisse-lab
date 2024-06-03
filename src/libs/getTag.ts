05f9f2c02672adfbc4852217077ea3d84576c223
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-06-04 00:16:17 +0900

    [fix]getTagを廃止し、getServersidePropsに変更

--- src/libs/getTag.ts
+++
@@ -1,6 +0,0 @@
-export const getTag = (pathname: string) => {
-  const segments = pathname.split("/").filter(Boolean);
-  const lastSegment = segments[segments.length - 1];
-  const categoryId = decodeURIComponent(lastSegment);
-  return categoryId;
-};
--- src/pages/user/[id]/index.page.tsx
+++ src/pages/user/[id]/index.page.tsx
@@ -3,12 +3,15 @@ import { GetServerSideProps } from "next";
 import { Concept } from "@/components/pages/common/Concept";
 import { Members } from "@/components/pages/common/Members";
 import { Recruit } from "@/components/pages/common/Recruit";
-import { AllPosts } from "@/components/pages/posts/AllPosts";
+import Profile from "@/components/pages/user/Profile/Profile";
+import { getUsers } from "@/libs/getUsers";
 
-const Page = ({ categoryId }: { categoryId: string }) => {
+const Page = ({ userId }: { userId: string }) => {
+  const user = getUsers().filter((user) => user.id === userId)[0];
+
   return (
     <>
-      <AllPosts categoryId={categoryId} />
+      <Profile user={user} />
       <Recruit />
       <Concept />
       <Members />
@@ -24,7 +27,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
     };
   return {
     props: {
-      categoryId: id,
+      userId: id,
     },
   };
 };
