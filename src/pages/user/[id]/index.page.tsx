876e34b8e18115ee8637252bc75f296586a6f002
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-06-04 00:57:01 +0900

    [feature]index.page.tsxにmemberProviderを実装

--- src/pages/user/[id]/index.page.tsx
+++ src/pages/user/[id]/index.page.tsx
@@ -3,15 +3,19 @@ import { GetServerSideProps } from "next";
 import { Concept } from "@/components/pages/common/Concept";
 import { Members } from "@/components/pages/common/Members";
 import { Recruit } from "@/components/pages/common/Recruit";
+import { ArchitectureWork } from "@/components/pages/user/ArchitectureWork";
 import Profile from "@/components/pages/user/Profile/Profile";
 import { getUsers } from "@/libs/getUsers";
+import { getWorks } from "@/libs/getWorks";
 
 const Page = ({ userId }: { userId: string }) => {
   const user = getUsers().filter((user) => user.id === userId)[0];
+  const archiWork = getWorks();
 
   return (
     <>
       <Profile user={user} />
+      <ArchitectureWork archiWork={archiWork} />
       <Recruit />
       <Concept />
       <Members />
