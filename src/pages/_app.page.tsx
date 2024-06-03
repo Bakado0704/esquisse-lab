661717f3b8148821deb333845e1d2f18d3342161
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-06-04 00:56:29 +0900

    [feature]memberProviderを実装

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,12 +1,15 @@
 import { HeaderLayout } from "@/components/layout/HeaderLayout";
+import { MemberProvider } from "@/contexts/member.context";
 import "@/styles/globals.scss";
 
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <HeaderLayout>
-      <Component {...pageProps} />
-    </HeaderLayout>
+    <MemberProvider>
+      <HeaderLayout>
+        <Component {...pageProps} />
+      </HeaderLayout>
+    </MemberProvider>
   );
 }
