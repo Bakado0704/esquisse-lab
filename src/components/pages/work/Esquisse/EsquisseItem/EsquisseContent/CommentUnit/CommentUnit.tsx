12e78652f4806ce22a342c9366b68a9c43e3be79
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-06-22 22:04:03 +0900

    [feature]commentコンポーネントの実装

---
+++ src/components/pages/work/Esquisse/EsquisseItem/EsquisseContent/CommentUnit/CommentUnit.tsx
@@ -0,0 +1,34 @@
+import { useFormContext } from 'react-hook-form';
+
+import { Button, FlexBox, TextArea } from '@/components/common';
+import { CommentFormValue } from '@/types/form/FirstForm.types';
+
+import styles from './CommentUnit.module.scss';
+
+const CommentUnit = () => {
+  const onComment = () => {};
+  const { register } = useFormContext<CommentFormValue>();
+
+  return (
+    <FlexBox flexDirection='column' justifyContent='center' gap='1.2rem'>
+      <TextArea
+        label='コメント追加'
+        rows={8}
+        placeholder='コメントを入力してください。'
+        {...register('comment')}
+      />
+      <FlexBox justifyContent='center' margin='1.2rem 0'>
+        <Button
+          theme='rectPink'
+          size='medium'
+          className={styles.button}
+          onClick={onComment}
+        >
+          コメントをする
+        </Button>
+      </FlexBox>
+    </FlexBox>
+  );
+};
+
+export default CommentUnit;
