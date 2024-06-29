23ec1dc022ea0ce7cd1995520f6cfdcd24dc8fc7
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-06-30 00:25:31 +0900

    [feature]buttonUnitのrouterを実装

--- src/components/pages/work/Esquisse/ButtonUnit/ButtonUnit.tsx
+++ src/components/pages/work/Esquisse/ButtonUnit/ButtonUnit.tsx
@@ -1,8 +1,15 @@
+import { useRouter } from 'next/navigation';
+
 import { FlexBox, Typography } from '@/components/common';
 
 import styles from './ButtonUnit.module.scss';
 
 const ButtonUnit = () => {
+  const router = useRouter();
+  const onCreateEsquisse = () => {
+    router.push(`/work/esquisse/new`);
+  };
+
   return (
     <FlexBox flexDirection='column' className={styles.container}>
       <FlexBox className={styles.lineContainer}>
@@ -15,7 +22,7 @@ const ButtonUnit = () => {
       </FlexBox>
       <FlexBox flexDirection='column' gap='1.2rem'>
         <FlexBox justifyContent='center'>
-          <button className={styles.button}>
+          <button className={styles.button} onClick={onCreateEsquisse}>
             <span />
             <span />
           </button>
