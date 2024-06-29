d1525b3065c540e731b7d03511afbf237d24c844
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-06-30 00:24:46 +0900

    [fix]form時にheaderのrouterを無効化する

--- src/components/pages/common/NavHeader/NavHeader.hooks.ts
+++ src/components/pages/common/NavHeader/NavHeader.hooks.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 import { useMemberContext } from '@/contexts/member.context';
 import { onScroll } from '@/hooks/useScroll';
@@ -8,15 +8,23 @@ import { getUsers } from '@/libs/getUsers';
 
 export const useNavHeader = () => {
   const router = useRouter();
+  const pathname = usePathname();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { members, setMembers } = useMemberContext();
   const users = getUsers();
+  const isFormPage = pathname.includes('/edit') || pathname.includes('/new');
 
   useEffect(() => {
     if (members.length) {
       onScroll('member', 'top');
     }
   }, [members]);
+
+  const onNavigateTop = () => {
+    if (!isFormPage) {
+      router.push('/home');
+    }
+  };
 
   const onMenuOpen = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -32,10 +40,11 @@ export const useNavHeader = () => {
   };
 
   return {
+    isFormPage,
     isMenuOpen,
     onMenuOpen,
+    onNavigateTop,
     onScrollMember,
     onScrollLogin,
-    router,
   };
 };
--- src/components/pages/common/NavHeader/NavHeader.tsx
+++ src/components/pages/common/NavHeader/NavHeader.tsx
@@ -10,8 +10,14 @@ import { useNavHeader } from './NavHeader.hooks';
 import styles from './NavHeader.module.scss';
 
 const NavHeader = () => {
-  const { isMenuOpen, onMenuOpen, onScrollMember, onScrollLogin, router } =
-    useNavHeader();
+  const {
+    isFormPage,
+    isMenuOpen,
+    onMenuOpen,
+    onNavigateTop,
+    onScrollMember,
+    onScrollLogin,
+  } = useNavHeader();
 
   return (
     <FlexBox
@@ -33,38 +39,45 @@ const NavHeader = () => {
           src={logo}
           alt='logo'
           height={40}
-          className={styles.logoImage}
-          onClick={() => router.push('/home')}
+          className={classNames(
+            styles.logoImage,
+            !isFormPage ? styles.activeLogoImage : undefined,
+          )}
+          onClick={onNavigateTop}
         />
-        <FlexBox className={styles.PcContainer}>
-          <Button
-            theme='textIndigo'
-            className={styles.memberButton}
-            onClick={onScrollMember}
-          >
-            Members
-          </Button>
-          <Button
-            theme='fill'
-            className={styles.loginButton}
-            onClick={onScrollLogin}
-          >
-            ログイン
-          </Button>
-        </FlexBox>
-        <FlexBox className={styles.SpContainer}>
-          <button
-            className={classNames(
-              styles.menuButton,
-              isMenuOpen ? styles.menuButtonOpen : undefined,
-            )}
-            onClick={onMenuOpen}
-          >
-            <span />
-            <span />
-            <span />
-          </button>
-        </FlexBox>
+        {!isFormPage && (
+          <FlexBox className={styles.PcContainer}>
+            <Button
+              theme='textIndigo'
+              className={styles.memberButton}
+              onClick={onScrollMember}
+            >
+              Members
+            </Button>
+            <Button
+              theme='fill'
+              className={styles.loginButton}
+              onClick={onScrollLogin}
+            >
+              ログイン
+            </Button>
+          </FlexBox>
+        )}
+        {!isFormPage && (
+          <FlexBox className={styles.SpContainer}>
+            <button
+              className={classNames(
+                styles.menuButton,
+                isMenuOpen ? styles.menuButtonOpen : undefined,
+              )}
+              onClick={onMenuOpen}
+            >
+              <span />
+              <span />
+              <span />
+            </button>
+          </FlexBox>
+        )}
       </FlexBox>
     </FlexBox>
   );
