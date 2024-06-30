1391177f5b0a55df9e4ea0c5a4bc3839f5d4a410
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-06-30 23:28:44 +0900

    [fix]Esquisseのcontextを削除

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,5 +1,4 @@
 import { HeaderLayout } from '@/components/layout/HeaderLayout';
-import { FormEsquisseProvider } from '@/contexts/formEsquisse.context';
 import { FormWorkProvider } from '@/contexts/formWork.context';
 import { MemberProvider } from '@/contexts/member.context';
 import '@/styles/globals.scss';
@@ -9,13 +8,11 @@ import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MemberProvider>
-      <FormEsquisseProvider>
-        <FormWorkProvider>
-          <HeaderLayout>
-            <Component {...pageProps} />
-          </HeaderLayout>
-        </FormWorkProvider>
-      </FormEsquisseProvider>
+      <FormWorkProvider>
+        <HeaderLayout>
+          <Component {...pageProps} />
+        </HeaderLayout>
+      </FormWorkProvider>
     </MemberProvider>
   );
 }
