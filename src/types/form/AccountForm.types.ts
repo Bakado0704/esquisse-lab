64432a494a0477df744938b9edba92f85579e0b1
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-07-01 23:23:45 +0900

    [feature]AccountFormSchemaの実装

---
+++ src/types/form/AccountForm.types.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+
+export const AccountFormSchema = z
+  .object({
+    name: z.string().min(1, '必須項目です'),
+    lab: z.string().min(1, '必須項目です'),
+    password1: z.string().min(1, '必須項目です'),
+    password2: z.string().min(1, '必須項目です'),
+  })
+  .superRefine((data, ctx) => {
+    if (data.password1 !== data.password2) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '同じパスワードを入力してください',
+        path: ['password2'],
+      });
+    }
+    if (data.password1.length <= 8) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '8文字以上のパスワードを入力してください',
+        path: ['password1'],
+      });
+    }
+    if (data.password2.length <= 8) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '8文字以上のパスワードを入力してください',
+        path: ['password2'],
+      });
+    }
+  });
+
+export type AccountFormValue = z.infer<typeof AccountFormSchema>;
