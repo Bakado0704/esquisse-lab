f29eec736d050c18dbb68942b5db9c5ceb2100cf
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-07-02 00:20:08 +0900

    [feature]accountとregisterのformの型を実装

--- src/types/form/AccountForm.types.ts
+++ src/types/form/AccountForm.types.ts
@@ -1,34 +1,17 @@
 import { z } from 'zod';
 
-export const AccountFormSchema = z
-  .object({
-    name: z.string().min(1, '必須項目です'),
-    lab: z.string().min(1, '必須項目です'),
-    password1: z.string().min(1, '必須項目です'),
-    password2: z.string().min(1, '必須項目です'),
-  })
-  .superRefine((data, ctx) => {
-    if (data.password1 !== data.password2) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: '同じパスワードを入力してください',
-        path: ['password2'],
-      });
-    }
-    if (data.password1.length <= 8) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: '8文字以上のパスワードを入力してください',
-        path: ['password1'],
-      });
-    }
-    if (data.password2.length <= 8) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: '8文字以上のパスワードを入力してください',
-        path: ['password2'],
-      });
-    }
-  });
+const isEmail = (value: string): boolean => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(value);
+};
+
+export const AccountFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, '必須項目です')
+    .refine((value) => isEmail(value), {
+      message: '正しい形式のメールアドレスを入力してください',
+    }),
+});
 
 export type AccountFormValue = z.infer<typeof AccountFormSchema>;
