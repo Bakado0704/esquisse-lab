0ef52fb34e86cd010fdef4ae0cc3460ed574717a
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-07-03 15:26:55 +0900

    [featrure]fvにアニメーションを付与

--- src/components/pages/home/Fv/Fv.tsx
+++ src/components/pages/home/Fv/Fv.tsx
@@ -3,32 +3,37 @@ import { useRouter } from 'next/navigation';
 
 import fvImg from '@/assets/fv/fv.png';
 import { Button, FlexBox, Icon, Typography } from '@/components/common';
+import { useFadeIn } from '@/hooks/useFadeIn';
 
 import styles from './Fv.module.scss';
 
 const Fv = () => {
   const router = useRouter();
+  useFadeIn({ targetId: 'fvImg', styles });
+  useFadeIn({ targetId: 'fvTitle', styles });
+
   return (
     <FlexBox className={styles.container}>
       <FlexBox className={styles.containerInner}>
-        <FlexBox className={styles.imageContainer}>
-          <Image
-            src={fvImg}
-            alt='fv'
-            style={{ objectFit: 'cover' }}
-            className={styles.image}
-          />
+        <FlexBox id='fvImg' className={styles.imageContainer}>
+          <Image alt='fv' src={fvImg} className={styles.image} />
         </FlexBox>
       </FlexBox>
       <FlexBox flexDirection='column' justifyContent='center' gap='2.4rem'>
-        <Typography gothic color='w1' className={styles.title}>
-          Hokudai
-          <br />
-          Esquisse
-          <br />
-          Forum
-        </Typography>
-        <Icon iconName='arrowLarge' />
+        <FlexBox
+          id='fvTitle'
+          flexDirection='column'
+          className={styles.titleContainer}
+        >
+          <Typography gothic color='w1' className={styles.title}>
+            Hokudai
+            <br />
+            Esquisse
+            <br />
+            Forum
+          </Typography>
+          <Icon iconName='arrowLarge' />
+        </FlexBox>
         <Button
           theme='rectPink'
           size='large'
