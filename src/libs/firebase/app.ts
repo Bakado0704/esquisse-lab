c4f6340c9c6da6d28cf64c5dd0f9b76fc4ea0b3f
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-07-04 19:59:33 +0900

    [feature]App.tsを実装

--- src/libs/firebase/app.ts
+++ src/libs/firebase/app.ts
@@ -1,5 +1,11 @@
 import { getApp, getApps, initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  signOut,
+  updateProfile,
+} from 'firebase/auth';
 import { getFirestore, initializeFirestore } from 'firebase/firestore';
 import { getFunctions } from 'firebase/functions';
 import { getStorage } from 'firebase/storage';
@@ -23,6 +29,10 @@ initializeFirestore(firebaseApp, {
 export const db = getFirestore(firebaseApp);
 export const functions = getFunctions(firebaseApp);
 export const auth = getAuth(firebaseApp);
+export const createUser = createUserWithEmailAndPassword;
+export const updateUser = updateProfile;
+export const sendEmail = sendEmailVerification;
+export const signout = signOut;
 export const storage = getStorage(firebaseApp);
 
 export default firebaseApp;
