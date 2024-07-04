5dbed83122326064d2e539cd835b40cc0626eaed
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-07-04 19:55:37 +0900

    [fix]IconImageコンポーネントを移動

---
+++ src/components/pages/register/IconImageInputUnit/IconImageInputUnit.hooks.ts
@@ -0,0 +1,60 @@
+import { useEffect, useRef, useState } from 'react';
+
+import { useFormContext } from 'react-hook-form';
+
+import { ImageDataType, ImageType } from '@/types/form/ImageForm.types';
+import { RegisterFormValue } from '@/types/form/RegisterForm.types';
+
+export const useIconImageInputUnit = () => {
+  const [iconImageData, setIconImageData] = useState<ImageDataType | null>(
+    null,
+  );
+  const ref = useRef<HTMLInputElement>(null);
+  const { setValue } = useFormContext<RegisterFormValue>();
+
+  const loadImage = (imgUrl: string) =>
+    new Promise<ImageType>((resolve) => {
+      const image = new Image();
+      image.src = imgUrl;
+      image.onload = () => {
+        const newSize = {
+          width: (image.width * 196) / image.height,
+          height: 196,
+        };
+        resolve({ objectUrl: imgUrl, imgSize: newSize });
+      };
+    });
+
+  const addImageHandler = async (file: File) => {
+    if (!file) return;
+
+    console.log('ddd');
+
+    const objectUrl = URL.createObjectURL(file);
+    loadImage(objectUrl)
+      .then((imageType) => {
+        const newImage = {
+          file: file,
+          objectUrl: objectUrl,
+          imgSize: imageType.imgSize,
+        };
+        console.log('ccc');
+        setIconImageData(newImage);
+      })
+      .catch((error) => {
+        console.error('Error loading images:', error);
+      });
+  };
+
+  useEffect(() => {
+    if (iconImageData) {
+      setValue('iconImageUrl', iconImageData.objectUrl, { shouldDirty: true });
+    }
+  }, [iconImageData]);
+
+  return {
+    ref,
+    iconImageData,
+    addImageHandler,
+  };
+};
---
+++ src/components/pages/register/IconImageInputUnit/IconImageInputUnit.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image';
+
+import { FlexBox } from '@/components/common';
+
+import { useIconImageInputUnit } from './IconImageInputUnit.hooks';
+import styles from './IconImageInputUnit.module.scss';
+
+const IconImageInputUnit = () => {
+  const { ref, iconImageData, addImageHandler } = useIconImageInputUnit();
+
+  return (
+    <FlexBox width='100%' flexDirection='column' gap='0.6rem'>
+      <FlexBox
+        justifyContent='center'
+        onClick={() => {
+          if (ref.current) {
+            ref.current.click();
+          }
+        }}
+      >
+        {iconImageData ? (
+          <FlexBox className={styles.imageContainer}>
+            <Image
+              src={iconImageData.objectUrl}
+              alt='アイコン画像'
+              fill
+              className={styles.image}
+            />
+          </FlexBox>
+        ) : (
+          <div className={styles.icon} />
+        )}
+      </FlexBox>
+      <input
+        onChange={(event) => {
+          const { files } = event.target;
+          if (!files) return;
+          addImageHandler(files[0]);
+        }}
+        type='file'
+        ref={ref}
+        hidden
+        value={undefined}
+        accept='image/png, image/jpeg, image/webp'
+        multiple={false}
+      />
+    </FlexBox>
+  );
+};
+
+export default IconImageInputUnit;
