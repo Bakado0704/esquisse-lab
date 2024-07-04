a99ff86ec992df8e042d9ce0014e9e6e34c5d348
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-07-04 19:52:15 +0900

    [fix]typeをAuthenticationに合わせる

--- src/types/form/AccountForm.types.ts
+++ src/types/form/AccountForm.types.ts
@@ -5,13 +5,39 @@ const isEmail = (value: string): boolean => {
   return emailRegex.test(value);
 };
 
-export const AccountFormSchema = z.object({
-  email: z
-    .string()
-    .min(1, '必須項目です')
-    .refine((value) => isEmail(value), {
-      message: '正しい形式のメールアドレスを入力してください',
-    }),
-});
+export const AccountFormSchema = z
+  .object({
+    email: z
+      .string()
+      .min(1, '必須項目です')
+      .refine((value) => isEmail(value), {
+        message: '正しい形式のメールアドレスを入力してください',
+      }),
+    password1: z.string().min(1, '必須項目です'),
+    password2: z.string().min(1, '必須項目です'),
+  })
+  .superRefine((data, ctx) => {
+    if (data.password1 !== data.password2) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '同じパスワードを入力してください',
+        path: ['password2'],
+      });
+    }
+    if (data.password1.length <= 8) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '8文字以上のパスワードを入力してください',
+        path: ['password1'],
+      });
+    }
+    if (data.password2.length <= 8) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: '8文字以上のパスワードを入力してください',
+        path: ['password2'],
+      });
+    }
+  });
 
 export type AccountFormValue = z.infer<typeof AccountFormSchema>;
