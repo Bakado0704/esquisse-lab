724e222262f204753afa4d0fb200d627fe795b35
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-07-05 12:53:50 +0900

    [fix]Userからuserにファイル名変更

--- src/types/application/userInfo.types.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const UserInfoSchema = z.object({
-  uid: z.string(),
-  name: z.string(),
-  email: z.string(),
-});
-
-export type UserInfo = z.infer<typeof UserInfoSchema>;
