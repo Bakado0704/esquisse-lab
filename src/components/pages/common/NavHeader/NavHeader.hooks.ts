a9aa0e1810eef9e6c6cd4778d155f5d271d027ac
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-07-05 17:20:04 +0900

    [feature]NavHeaderをログインログアウト機能を付与

--- src/components/pages/common/NavHeader/NavHeader.hooks.ts
+++ src/components/pages/common/NavHeader/NavHeader.hooks.ts
@@ -2,17 +2,24 @@ import { useEffect, useState } from 'react';
 
 import { usePathname, useRouter } from 'next/navigation';
 
+import { useAuthContext } from '@/contexts/auth.context';
 import { useMemberContext } from '@/contexts/member.context';
 import { clearScroll, onScroll } from '@/hooks/useScroll';
 import { getUsers } from '@/libs/getUsers';
+import { Logout } from '@/libs/service/form/authentication/logout';
 
 export const useNavHeader = () => {
   const router = useRouter();
   const pathname = usePathname() || '';
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { members, setMembers } = useMemberContext();
+  const { user, setUser } = useAuthContext();
   const users = getUsers();
-  const isFormPage = pathname.includes('/edit') || pathname.includes('/new');
+  const isFormPage =
+    pathname.includes('/edit') ||
+    pathname.includes('/new') ||
+    pathname.includes('/account') ||
+    pathname.includes('/register');
 
   useEffect(() => {
     if (members.length) {
@@ -30,6 +37,13 @@ export const useNavHeader = () => {
     }
   };
 
+  const onLogout = () => {
+    Logout().then(() => {
+      setUser(undefined);
+      alert('ログアウトしました');
+    });
+  };
+
   const onMenuOpen = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -42,12 +56,18 @@ export const useNavHeader = () => {
     onScroll('login', 'top');
     setIsMenuOpen(false);
   };
+  const onNavigateUser = ({ userId }: { userId: string }) => {
+    router.push(`/user/${userId}`);
+  };
 
   return {
+    user,
     isFormPage,
     isMenuOpen,
+    onLogout,
     onMenuOpen,
     onNavigateTop,
+    onNavigateUser,
     onScrollMember,
     onScrollLogin,
   };
