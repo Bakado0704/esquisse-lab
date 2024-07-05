06babddc80055e7f73ec004f2e712c14bf576ea3
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-07-05 17:20:22 +0900

    [feature]NavMenuにログインログアウト機能を付与

--- src/components/pages/common/NavMenu/NavMenu.tsx
+++ src/components/pages/common/NavMenu/NavMenu.tsx
@@ -6,44 +6,84 @@ import styles from './NavMenu.module.scss';
 import { NavMenuProps } from './NavMenu.types';
 
 const NavMenu = ({
+  user,
   isMenuOpen,
+  onLogout,
   onScrollMember,
   onScrollLogin,
+  onNavigateUser,
 }: NavMenuProps) => {
   return (
     <FlexBox
       flexDirection='column'
+      justifyContent='space-between'
       gap='2.4rem'
       className={classNames(
         styles.container,
         isMenuOpen ? styles.open : undefined,
       )}
     >
-      <Button
-        theme='textIndigo'
-        className={styles.button}
-        onClick={onScrollMember}
-      >
-        <FlexBox justifyContent='left' gap='1.2rem' alignItems='center'>
-          <Typography gothic fontSize='2.4rem' fontWeight={600}>
-            Members
-          </Typography>
-          <Typography fontSize='1.6rem'>メンバー</Typography>
-        </FlexBox>
-      </Button>
+      <FlexBox flexDirection='column' gap='2.4rem'>
+        <Button
+          theme='textIndigo'
+          className={styles.button}
+          onClick={onScrollMember}
+        >
+          <FlexBox justifyContent='left' gap='1.2rem' alignItems='center'>
+            <Typography gothic fontSize='2.4rem' fontWeight={600}>
+              Members
+            </Typography>
+            <Typography fontSize='1.6rem'>メンバー</Typography>
+          </FlexBox>
+        </Button>
 
-      <Button
-        theme='textIndigo'
-        className={styles.button}
-        onClick={onScrollLogin}
-      >
-        <FlexBox justifyContent='left' gap='1.2rem' alignItems='center'>
-          <Typography gothic fontSize='2.4rem' fontWeight={600}>
-            Login
-          </Typography>
-          <Typography fontSize='1.6rem'>ログイン</Typography>
-        </FlexBox>
-      </Button>
+        {!user && (
+          <Button
+            theme='textIndigo'
+            className={styles.button}
+            onClick={onScrollLogin}
+          >
+            <FlexBox justifyContent='left' gap='1.2rem' alignItems='center'>
+              <Typography gothic fontSize='2.4rem' fontWeight={600}>
+                Login
+              </Typography>
+              <Typography fontSize='1.6rem'>ログイン</Typography>
+            </FlexBox>
+          </Button>
+        )}
+
+        {user && (
+          <Button
+            theme='textIndigo'
+            className={styles.button}
+            onClick={() => {
+              onNavigateUser({ userId: user.id });
+            }}
+          >
+            <FlexBox justifyContent='left' gap='1.2rem' alignItems='center'>
+              <Typography gothic fontSize='2.4rem' fontWeight={600}>
+                Account
+              </Typography>
+              <Typography fontSize='1.6rem'>アカウント</Typography>
+            </FlexBox>
+          </Button>
+        )}
+      </FlexBox>
+
+      {user && (
+        <Button
+          theme='textIndigo'
+          className={styles.logoutButton}
+          onClick={onLogout}
+        >
+          <FlexBox justifyContent='left' gap='1.2rem' alignItems='center'>
+            <Typography gothic fontSize='2.4rem' fontWeight={600}>
+              Logout
+            </Typography>
+            <Typography fontSize='1.6rem'>ログアウト</Typography>
+          </FlexBox>
+        </Button>
+      )}
     </FlexBox>
   );
 };
