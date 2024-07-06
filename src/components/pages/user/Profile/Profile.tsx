1723f62fc55cb4ca622c74deca1a8ac32a448c72
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-07-06 14:49:35 +0900

    [fix]Profileの背景画像を設定

--- src/components/pages/user/Profile/Profile.tsx
+++ src/components/pages/user/Profile/Profile.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { FlexBox, Typography } from '@/components/common';
 
 import styles from './Profile.module.scss';
@@ -10,7 +12,18 @@ const Profile = ({ user }: ProfileProps) => {
   return (
     <FlexBox gap='3.2rem' flexDirection='column' className={styles.container}>
       <FlexBox justifyContent='center' flexDirection='column' gap='1.2rem'>
-        <div className={styles.bg} />
+        <FlexBox className={styles.bgContainer}>
+          {user && user.coverImageUrl ? (
+            <Image
+              alt='bg'
+              fill
+              src={user.coverImageUrl}
+              className={styles.coverImage}
+            />
+          ) : (
+            <div className={styles.bg} />
+          )}
+        </FlexBox>
         <UserIcon user={user} />
         <FlexBox justifyContent='center' flexDirection='column' gap='2.4rem'>
           <FlexBox flexDirection='column' gap='0.8rem'>
@@ -22,7 +35,7 @@ const Profile = ({ user }: ProfileProps) => {
             </Typography>
           </FlexBox>
 
-          <Typography fontSize='1.2rem' fontWeight={600} textAlign='center'>
+          <Typography fontSize='1.2rem' color='b2' textAlign='center'>
             {user.detail}
           </Typography>
         </FlexBox>
