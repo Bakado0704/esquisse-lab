b19d53b0b972dc59859acbf2ff369e359f14c31e
Bakado0704 <kado_hiroki@yahoo.co.jp>
2024-09-21 21:06:30 +0900

    [fix]ポートフォリオ部分のUserIcon置き換え

--- src/components/pages/user/PersonalProject/TextContainerPc/TextContainerPc.tsx
+++ src/components/pages/user/PersonalProject/TextContainerPc/TextContainerPc.tsx
@@ -1,8 +1,12 @@
 import { FlexBox, Typography } from '@/components/common';
+import { UserIcon } from '@/components/pages/common/UserIcon';
 
 import styles from './TextContainerPc.module.scss';
+import { useIconImage } from './useIconImage.hooks';
 
 const TextContainerPc = () => {
+  const { iconImageUrl } = useIconImage();
+
   return (
     <FlexBox flexDirection='column' gap='2rem' className={styles.container}>
       <FlexBox flexDirection='column' gap='0.4rem'>
@@ -17,7 +21,12 @@ const TextContainerPc = () => {
         建築学科のオンラインのエスキスサービスです。オンライン上で設計課題のエスキスができます。完成品ではなく、過程を投稿することによって、作品の移り変わりが記録されるようになっており、「他の人の作品制作の過程が知りたい」「先輩TAと作品について議論したい」という課題を解決します。
       </Typography>
       <FlexBox gap='0.8rem' alignItems='center'>
-        <div className={styles.icon} />
+        <UserIcon
+          iconImageUrl={iconImageUrl}
+          size='3rem'
+          isRouterActive={false}
+          href=''
+        />
         <Typography fontSize='1.2rem' color='w1'>
           Kado Hiroki
         </Typography>
